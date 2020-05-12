da2a1f0e30a1922c7d069cce92baa1a7401624a0
jelarson <jesslarson93@gmail.com>
2020-05-12 17:01:41 -0600

    started media queries on login and new account page

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -85,10 +85,10 @@ export default function Login(props) {
   return (
     <div className='login-container'>
       {/* <img src={Logo} alt='Logo' /> */}
-      <div className='login-image' style={{ backgroundImage: 'url(https://thumbs.dreamstime.com/t/escola-de-protuber%C3%A2ncia-tocantes-do-livro-cego-doente-braile-da-leitura-crian%C3%A7a-113604556.jpg)' }} />
+      <div className='login-image login-image-one' style={{ backgroundImage: 'url(https://thumbs.dreamstime.com/t/escola-de-protuber%C3%A2ncia-tocantes-do-livro-cego-doente-braile-da-leitura-crian%C3%A7a-113604556.jpg)' }} />
       <div className='login-image' style={{ backgroundImage: 'url(https://img.freepik.com/free-photo/two-women-speak-sign-language-girls-talk-language-hearing-impaired-people-deaf_8119-2247.jpg?size=626&ext=jpg)' }} />
-      <div className='login-image' style={{ backgroundImage: 'url(https://www.stronggo.com/sites/default/files/styles/image_col_12/public/2018-12/ada-blind-people-etiquette.jpg?itok=NHGJzVdu)' }} />
-      <div className='login-image' style={{ backgroundImage: 'url(https://imgix.ranker.com/user_node_img/50067/1001323114/original/people-talking-slowly-which-throws-off-their-lip-reading-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces)' }} />
+      <div className='login-image login-image-one' style={{ backgroundImage: 'url(https://www.stronggo.com/sites/default/files/styles/image_col_12/public/2018-12/ada-blind-people-etiquette.jpg?itok=NHGJzVdu)' }} />
+      <div className='login-image login-image-one' style={{ backgroundImage: 'url(https://imgix.ranker.com/user_node_img/50067/1001323114/original/people-talking-slowly-which-throws-off-their-lip-reading-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces)' }} />
       <div className='login-container-content'>
 
 
@@ -143,10 +143,10 @@ export default function Login(props) {
           </div>
         </form>
       </div>
-      <div className='login-image' style={{ backgroundImage: 'url(https://images1.westword.com/imager/u/745xauto/10774837/istock-deaf-sign-language.jpg)' }} />
-      <div className='login-image' style={{ backgroundImage: 'url(https://image.freepik.com/foto-gratis/senior-hombre-ciego-leyendo-libro-braille_107420-34787.jpg)' }} />
+      <div className='login-image login-image-one' style={{ backgroundImage: 'url(https://images1.westword.com/imager/u/745xauto/10774837/istock-deaf-sign-language.jpg)' }} />
+      <div className='login-image login-image-one' style={{ backgroundImage: 'url(https://image.freepik.com/foto-gratis/senior-hombre-ciego-leyendo-libro-braille_107420-34787.jpg)' }} />
       <div className='login-image' style={{ backgroundImage: 'url(https://www.columbusspeech.org/wp-content/uploads/2018/02/shutterstock_1011270694-2experiment.jpg)' }} />
-      <div className='login-image' style={{ backgroundImage: 'url(https://www.perkinselearning.org/sites/elearning.perkinsdev1.org/files/reading_braille2.jpg)' }} />
+      <div className='login-image login-image-one' style={{ backgroundImage: 'url(https://www.perkinselearning.org/sites/elearning.perkinsdev1.org/files/reading_braille2.jpg)' }} />
     </div>
   );
 }
