ac01165c02b3ecc0a06055891e0f1e551aacb040
jelarson <jesslarson93@gmail.com>
2020-05-13 00:05:19 -0600

    added media queries to all main pages

--- src/components/new-account/new-account.js
+++ src/components/new-account/new-account.js
@@ -48,10 +48,10 @@ export default function NewAccount(props) {
 
   return (
     <div className='new-account-container'>
-      <div className='new-acct-image' style={{ backgroundImage: 'url(https://thumbs.dreamstime.com/t/escola-de-protuber%C3%A2ncia-tocantes-do-livro-cego-doente-braile-da-leitura-crian%C3%A7a-113604556.jpg)' }} />
+      <div className='new-acct-image new-account-image-one' style={{ backgroundImage: 'url(https://thumbs.dreamstime.com/t/escola-de-protuber%C3%A2ncia-tocantes-do-livro-cego-doente-braile-da-leitura-crian%C3%A7a-113604556.jpg)' }} />
       <div className='new-acct-image' style={{ backgroundImage: 'url(https://img.freepik.com/free-photo/two-women-speak-sign-language-girls-talk-language-hearing-impaired-people-deaf_8119-2247.jpg?size=626&ext=jpg)' }} />
-      <div className='new-acct-image' style={{ backgroundImage: 'url(https://www.stronggo.com/sites/default/files/styles/image_col_12/public/2018-12/ada-blind-people-etiquette.jpg?itok=NHGJzVdu)' }} />
-      <div className='new-acct-image' style={{ backgroundImage: 'url(https://imgix.ranker.com/user_node_img/50067/1001323114/original/people-talking-slowly-which-throws-off-their-lip-reading-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces)' }} />
+      <div className='new-acct-image new-account-image-one' style={{ backgroundImage: 'url(https://www.stronggo.com/sites/default/files/styles/image_col_12/public/2018-12/ada-blind-people-etiquette.jpg?itok=NHGJzVdu)' }} />
+      <div className='new-acct-image new-account-image-one' style={{ backgroundImage: 'url(https://imgix.ranker.com/user_node_img/50067/1001323114/original/people-talking-slowly-which-throws-off-their-lip-reading-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces)' }} />
       <div className='new-account-container-content'>
 
 
@@ -110,10 +110,10 @@ export default function NewAccount(props) {
           </div>
         </form>
       </div>
-      <div className='new-acct-image' style={{ backgroundImage: 'url(https://images1.westword.com/imager/u/745xauto/10774837/istock-deaf-sign-language.jpg)' }} />
-      <div className='new-acct-image' style={{ backgroundImage: 'url(https://image.freepik.com/foto-gratis/senior-hombre-ciego-leyendo-libro-braille_107420-34787.jpg)' }} />
+      <div className='new-acct-image new-account-image-one' style={{ backgroundImage: 'url(https://images1.westword.com/imager/u/745xauto/10774837/istock-deaf-sign-language.jpg)' }} />
+      <div className='new-acct-image new-account-image-one' style={{ backgroundImage: 'url(https://image.freepik.com/foto-gratis/senior-hombre-ciego-leyendo-libro-braille_107420-34787.jpg)' }} />
       <div className='new-acct-image' style={{ backgroundImage: 'url(https://www.columbusspeech.org/wp-content/uploads/2018/02/shutterstock_1011270694-2experiment.jpg)' }} />
-      <div className='new-acct-image' style={{ backgroundImage: 'url(https://www.perkinselearning.org/sites/elearning.perkinsdev1.org/files/reading_braille2.jpg)' }} />
+      <div className='new-acct-image new-account-image-one' style={{ backgroundImage: 'url(https://www.perkinselearning.org/sites/elearning.perkinsdev1.org/files/reading_braille2.jpg)' }} />
     </div>
   );
 }
